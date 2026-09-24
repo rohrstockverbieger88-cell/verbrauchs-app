dc105beddb9ec0bac6107479a51dd6ca95bbac92
Bjoern Herold <bjoern@heroldweb.de>
2026-09-24 23:53:19 +0200

    Fix: Export syntax in Vercel function and bumped cache

--- api/contact.js
+++ api/contact.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'verbrauch-v29';
+const CACHE_NAME = 'verbrauch-v30';
 const urlsToCache = [
   './',
   './app.html',
